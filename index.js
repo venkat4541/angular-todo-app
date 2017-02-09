38c404b8cddee7c54f111430b278176ece9e072b
Venkat Polumahanti <venkat4541@gmail.com>
2017-02-08 22:39:05 -0500

    Tasks are now editable

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var appX = angular.module('mainApp', []);
 
 // Controller
 appX.controller('app', function($scope) {
+
   $scope.tasks = [];
   $scope.task="";
+
   $scope.searchEnter = function() {
     if(event.which == 13 && $scope.task != "") {
       $scope.addTask();
@@ -15,5 +17,19 @@ appX.controller('app', function($scope) {
      'taskDone': 'false'});
     console.log($scope.tasks);
     $scope.task="";
-  }
+  };
+
+  $scope.taskEdit = function() {
+    console.log(event.target.contentEditable);
+    event.target.contentEditable =
+      event.target.contentEditable == "false" ? "true" : "false";
+    console.log(event.target.contentEditable);
+  };
+
+  $scope.editDone = function(name) {
+    if(event.which == 13 && name != "") {
+      $scope.taskEdit();
+    }
+
+  };
 });
